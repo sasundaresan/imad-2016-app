6430b68e7f6d39d3d4a235684118b50895d1f5b0
S A Sundaresan <msk_sundu@yahoo.co.in>
2016-09-22 21:21:11 +0530

    client side js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,28 @@
 console.log('Loaded!');
+var marginLeft = 0;
+function moveRight() {
+	marginLeft += 2;
+	img.style.marginLeft = marginLeft + 'px';
+}
+
+var img = document.getElementById('pict');
+img.onclick = function() {
+	var interv = setInterval(moveRight, 50);
+}
+
+var doc = document.getElementById('main_text');
+var orig = true;
+
+function changeText() {
+	if (orig) {
+		doc.innerHTML = "<P>This is original text!</P>";
+	}
+	else {
+		doc.innerHTML = "<P>This is changed text!</P>";
+	}
+	orig = !orig;
+}
+
+doc.onclick = function() {
+	var sirv = setInterval(changeText, 2000);
+}
