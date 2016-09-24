48939b79b5f8f0e78f8a61db0fd71caace855fae
S A Sundaresan <msk_sundu@yahoo.co.in>
2016-09-24 08:37:08 +0530

    clear interval added, toggle left/right

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,57 @@
 console.log('Loaded!');
+
+// This moves the picture (element pict) to the right/left steadily when clicked
+// will stop when reaches the end
 var marginLeft = 0;
+var right = false;
+var interv;
 function moveRight() {
-	marginLeft += 2;
+	if (right) {
+		if (marginLeft >= 150) {
+			clearInterval(interv);
+		}
+		else {
+			marginLeft += 2;
+		}
+	}
+	else {
+		if (marginLeft <= 0) {
+			clearInterval(interv);
+		}
+		else {
+			marginLeft -= 2;
+		}
+	}
 	img.style.marginLeft = marginLeft + 'px';
 }
 
 var img = document.getElementById('pict');
 img.onclick = function() {
-	var interv = setInterval(moveRight, 50);
+	right = !right;
+	interv = setInterval(moveRight, 50);
 }
 
+// This keeps flipping the text (element main_text) when clicked 10 times
 var doc = document.getElementById('main_text');
 var orig = true;
+var sirv;
+var count;
 
 function changeText() {
+	if (count > 10) {
+		clearInterval(sirv);
+	}
 	if (orig) {
-		doc.innerHTML = "<P>This is original text!</P>";
+		doc.innerHTML = "<P>This is original text! (Count=" + count + ")</P>";
 	}
 	else {
-		doc.innerHTML = "<P>This is changed text!</P>";
+		count++;
+		doc.innerHTML = "<P>This is changed text! (Count=" + count + ")</P>";
 	}
 	orig = !orig;
 }
 
 doc.onclick = function() {
-	var sirv = setInterval(changeText, 2000);
+	count = 0;
+	sirv = setInterval(changeText, 2000);
 }
