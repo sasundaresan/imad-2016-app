1a6332b5e762fb3b78f01fb9d2e4dba1042e9447
sasundaresan <sasundaresan@users.noreply.github.com>
2016-09-24 09:15:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,6 +79,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var countBtnNo=0;
+app.get('/counter', function(req, res) {
+    countBtnNo++;
+    res.send(countBtnNo.toString());
+});
+
 app.get('/:articleName', function(req, res) {
 	var articleName = req.params.articleName;
 	res.send(createTemplate(articles[articleName]));
