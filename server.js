45ab353112b46511ccc7bb3e8265a7965b410f3b
sasundaresan <sasundaresan@users.noreply.github.com>
2016-11-06 11:40:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get('/jokes/:articleName', function(req, res) {
 		if (err) {
 			res.status(500).send(err.toString());
 		} else {
-			if (result.rows.length == 0) {
+			if (result.rows.length === 0) {
 				res.status(404).send('Joke Not Found!');
 			} else {
 				var articleData = result.rows[0];
