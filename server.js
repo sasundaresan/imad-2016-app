d5c3e4072e8d5b4d8292bfe866dc8a5bb7882ccd
sasundaresan <sasundaresan@users.noreply.github.com>
2016-11-07 21:02:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ app.get('/check-login', function(req, res) {
 app.get('/logout', function(req, res) {
 	delete req.session.auth;
 	res.send('You are logged out!');
-}
+});
 
 var countBtnNo=0;
 app.get('/counter', function(req, res) {
